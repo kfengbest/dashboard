8813e91e6cf1629ffd393943646bb57117d38b9b
kaven feng Application Admin <fengkaadmin@ads.autodesk.com>
2017-11-30 21:05:04 -0800

    inheritance is working

--- src/app/data_view_application.js
+++ src/app/data_view_application.js
@@ -1,13 +1,17 @@
 goog.provide('neutron.dashboard.DataViewApplication');
 
 goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
 goog.require('goog.events.EventTarget');
 
 
 neutron.dashboard.DataViewApplication = function(){
     console.log("DataViewApplication")
+    goog.base(this);
 }
 
+goog.inherits(neutron.dashboard.DataViewApplication, goog.events.EventTarget);
 
 function DataViewApplicationStart(){
     console.log("DataViewApplicationStart")    
